1a812874a7f07afce0353cbd6e00a9ac7eedcaa5
annaviper <elvereth@gmail.com>
2020-04-19 00:18:09 +0100

    updating path to datasets in exploratory

--- src/pages/exploratory.py
+++ src/pages/exploratory.py
@@ -8,15 +8,12 @@ def write():
     # with st.spinner("Loading Home ..."):
 
     @st.cache
-    def load_data(URL, nrows):
-        data = pd.read_csv(URL, nrows=nrows)
-        # def lowercase(x): return str(x).lower()
-        # data.rename(lowercase, axis='columns', inplace=True)
-        return data
+    def load_data(URL):
+        return pd.read_csv(URL)
 
     # data_load_state = st.text('Loading data...')
     URL = 'datasets/kepler_processed.csv'
-    df = load_data(URL, 4000)
+    df = load_data(URL)
     # data_load_state.text('Loading data...done')
 
     # DISPOSITION
@@ -35,7 +32,7 @@ def write():
     st.info("'The square of the orbital period of a planet is directly proportional to the cube of the semi-major axis of its orbit.'")
 
     URL = 'datasets/phl_new_processed.csv'
-    df = load_data(URL, 4000)
+    df = load_data(URL)
 
     df_kepler = df[['p_period', 'p_semi_major_axis']].copy()
     df_kepler.dropna(inplace=True)
@@ -44,8 +41,8 @@ def write():
 
     fig = px.scatter(df_kepler, x='period_squared', y='p_semi_major_axis_cubed', trendline='ols')
     fig.update_layout(
-        xaxis_title_text='Orbital period ^ 2',
-        yaxis_title_text='Semi-major axis ^ 3',
+        xaxis_title_text='Orbital period ^ 2 (days)',
+        yaxis_title_text='Semi-major axis ^ 3 (AU)',
         showlegend=False
     )
     fig.update_xaxes(range=[0, 6500])
@@ -54,7 +51,7 @@ def write():
 
     from scipy import stats
     slope, intercept, r_value, p_value, std_err = stats.linregress(df_kepler['period_squared'], df_kepler['p_semi_major_axis_cubed'])
-    st.write('The R squared is ', r_value**2)
+    st.write('The R squared is ', round(r_value**2,4), '!')
 
     # TYPE OF PLANETS
     st.title('Types of exoplanets')
@@ -71,9 +68,9 @@ def write():
     df_planet = df[['p_mass', 'p_radius', 'p_type']].dropna()
     fig = px.scatter(df_planet, x="p_mass", y='p_radius', opacity=0.8, color='p_type')
     fig.update_layout(
-        xaxis_title_text='Mass',
-        yaxis_title_text='Radius'
+        xaxis_title_text='Mass (EU)',
+        yaxis_title_text='Radius (ER)'
     )
-    fig.update_xaxes(range=[0, 80])
-    fig.update_yaxes(range=[0, 10])
+    fig.update_xaxes(range=[-0.1, 80])
+    fig.update_yaxes(range=[-0.05, 10])
     st.plotly_chart(fig)
